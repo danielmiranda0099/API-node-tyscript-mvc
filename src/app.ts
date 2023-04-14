d7e463ee4fde7939b02e3b1134db38deea695655
daniel <danielmesa877@gmail.com>
2023-04-13 23:30:29 -0500

    [ADD] Setup DB MongoDb

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-import "dotenv/config"
-import express from "express"
-import cors from "cors"
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
 
 import { router } from "./routes";
+import { dbConnection as DB } from "./config/mongo";
 
-const PORT = process.env.PORT  || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use( cors() );
 app.use( router );
+
+DB().then( () => console.log("DB -------> OK"));
 
 app.listen(PORT, () => console.log('server -----> OK'));
